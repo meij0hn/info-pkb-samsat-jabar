9bd7a282d3f461fabe28e0155fff9a2b2dc8cc02
meijohn <meijohnnura@gmail.com>
2025-12-15 10:43:53 +0700

    fix: error origin

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,7 +41,7 @@ export function middleware(request: NextRequest) {
             response.headers.set('Access-Control-Allow-Origin', origin);
         }
 
-        response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+        response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS, POST');
         response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         response.headers.set('Access-Control-Max-Age', '86400'); // 24 hours
 
